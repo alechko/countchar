e977f00d764dd86cf28e0ea9eb6531e9453f4404
alechko <mail@alechko.net>
2013-10-23 16:27:30 +0200

    fire onload.

--- counter.js
+++ counter.js
@@ -1,16 +1,14 @@
 (function() {
-    (function check() {
-        window.onload = function() {        
-            var ev = document.body;
-            var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';                            
-            var length = getSelectionText().length > 0 ? getSelectionText().length.toString() : '0';
-            chrome[roe].sendMessage({count: length});
-            // Set count on mouseup
-            ev.addEventListener('mouseup', function(e) {
-                chrome[roe].sendMessage({count: getSelectionText().length.toString()});
-            });
-        }
-    })();
+    window.onload = function() {
+        var ev = document.body;
+        var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';                            
+        var length = getSelectionText().length > 0 ? getSelectionText().length.toString() : '0';
+        chrome[roe].sendMessage({count: length});
+        // Set count on mouseup
+        ev.addEventListener('mouseup', function(e) {
+            chrome[roe].sendMessage({count: getSelectionText().length.toString()});
+        });
+    };
     function getSelectionText() {
         var text = "";
         if (window.getSelection) {
@@ -18,5 +16,4 @@
         }
         return text;
     }
-
 })();
