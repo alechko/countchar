c9dad5b111c1ce6fd6b26c8772836d43c33d8a05
alechko <mail@alechko.net>
2013-10-23 16:37:32 +0200

    improving.

--- counter.js
+++ counter.js
@@ -1,13 +1,13 @@
 (function() {
+    var ev = window;
+    var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';                            
+    var length = getSelectionText().length > 0 ? getSelectionText().length.toString() : '0';
+    chrome[roe].sendMessage({count: length});
+    // Set count on mouseup
+    ev.addEventListener('mouseup', function(e) {
+        chrome[roe].sendMessage({count: getSelectionText().length.toString()});
+    });
     window.onload = function() {
-        var ev = document.body;
-        var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';                            
-        var length = getSelectionText().length > 0 ? getSelectionText().length.toString() : '0';
-        chrome[roe].sendMessage({count: length});
-        // Set count on mouseup
-        ev.addEventListener('mouseup', function(e) {
-            chrome[roe].sendMessage({count: getSelectionText().length.toString()});
-        });
     };
     function getSelectionText() {
         var text = "";
