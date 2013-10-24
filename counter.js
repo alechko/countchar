40adf09b38683ed40e11f4b0e97a34cd7dfaff74
alechko <mail@alechko.net>
2013-10-24 14:37:04 +0200

    removing unnececary code.

--- counter.js
+++ counter.js
@@ -1,19 +1,17 @@
 (function() {
+    // Set message once page loaded
+    var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
+    chrome[roe].sendMessage({count: setMessage()});
+    // Set message on mouseup event
     var ev = window;
-    var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';                            
-    var length = getSelectionText().length > 0 ? getSelectionText().length.toString() : '0';
-    chrome[roe].sendMessage({count: length});
-    // Set count on mouseup
     ev.addEventListener('mouseup', function(e) {
-        chrome[roe].sendMessage({count: getSelectionText().length.toString()});
+        chrome[roe].sendMessage({count: setMessage()});
     });
-    window.onload = function() {
-    };
-    function getSelectionText() {
-        var text = "";
+    function setMessage(){
+        var message = '';
         if (window.getSelection) {
-            text = window.getSelection().toString();
-        }
-        return text;
+            message = window.getSelection().toString().length.toString();
+        }        
+        return message;       
     }
 })();
